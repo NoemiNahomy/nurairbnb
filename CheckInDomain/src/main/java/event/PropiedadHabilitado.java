8faa155348eb4ad31064ce458dbc15ef24a89ad9
Noemi Ticona <noemi.t.angles@gmail.com>
2024-01-21 10:11:48 -0400

    add lintstage

--- CheckInDomain/src/main/java/event/PropiedadHabilitado.java
+++ CheckInDomain/src/main/java/event/PropiedadHabilitado.java
@@ -5,7 +5,6 @@ import java.util.Date;
 import model.Propiedad;
 
 public class PropiedadHabilitado extends DomainEvent {
-
   public Propiedad propiedadHabilitada;
 
   public PropiedadHabilitado(Propiedad propiedadHabilitada, Date occuredOn) {
